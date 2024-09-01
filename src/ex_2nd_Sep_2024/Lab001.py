e7c02ccf07860aadad8fa5a59c1f8f33c945ef0b
neetusingh <neetusingh3699@gmail.com>
2024-09-02 02:23:10 +0530

    Lab002 Added

---
+++ src/ex_2nd_Sep_2024/Lab001.py
@@ -0,0 +1,13 @@
+# This is a Comment - which dowsn't run
+
+print("Hello Neetu, Welcome to Automation Python Learning.")
+print("Hello Neetu, Welcome to Automation Python Learning.") # duplicating by Command+D in ma & Ctrl+D in windows
+print(2+2)
+print(2-2)
+
+# in case of re-run the program - shortcut - Command+R
+# https://resources.jetbrains.com/storage/products/pycharm/docs/PyCharm_ReferenceCard_mac.pdf
+
+
+
+
